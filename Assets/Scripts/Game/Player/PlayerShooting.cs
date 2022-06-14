0437f6fd4c0c288fbccba9f753cbb93963d29777
MateuszHildebrandt <mateuszhildebrandt@outlook.com>
2022-06-14 15:37:58 +0200

    Added input system for PlayerShooting. Fixed bullet position.

--- Assets/Scripts/Game/Player/PlayerShooting.cs
+++ Assets/Scripts/Game/Player/PlayerShooting.cs
@@ -3,7 +3,7 @@ using UnityEngine.InputSystem;
 
 namespace Player
 {
-    public class PlayerShooting : MonoBehaviour
+    public class PlayerShooting : MonoBehaviour, IInputActionsReceiver
     {
         [Header("Settings")]
         [SerializeField] float bulletForce = 10f;
@@ -15,15 +15,17 @@ namespace Player
 
         private float _bulletOffset = 0.2f;
 
+        public InputActions InputActions { get; private set; }
+
         private void Awake()
         {
             _camera = Camera.main;
             _spriteRenderer = GetComponent<SpriteRenderer>();
         }
 
-        private void Update()
+        private void Start()
         {
-            //TODO: shoot on click
+            InputActions.Player.Fire.performed += (_) => Shoot();
         }
 
         [ExposeMethodInEditor]
@@ -31,26 +33,29 @@ namespace Player
         {
             Vector2 mouseWorldPoint = _camera.ScreenToWorldPoint(Mouse.current.position.ReadValue());
             Vector2 direction = mouseWorldPoint - (Vector2)transform.position;
-            Debug.DrawRay(transform.position, direction, Color.red);
+            //Debug.DrawRay(transform.position, direction, Color.red);
             GameObject bullet = Instantiate(bulletPrefab, SetBulletPosition(direction), Quaternion.identity, transform);
-            bullet.GetComponent<Rigidbody2D>().AddForce(direction * bulletForce, ForceMode2D.Impulse);
+            bullet.GetComponent<Rigidbody2D>().AddForce(direction.normalized * bulletForce, ForceMode2D.Impulse);
             Destroy(bullet, 5);
         }
 
         private Vector3 SetBulletPosition(Vector2 direction)
         {
             Bounds bounds = _spriteRenderer.bounds;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
             //4 directions
-            if (direction.x > 0)
+            if (angle >= -45 && angle <= 45) //Right
                 return new Vector2(bounds.max.x + _bulletOffset, bounds.center.y);
-            else if (direction.x < 0)
-                return new Vector2(bounds.min.x - _bulletOffset, bounds.center.y);
-            else if (direction.y > 0)
-                return new Vector2(bounds.center.x + _bulletOffset, bounds.max.y);
-            else
+            else if (angle > 45 && angle < 135) //Up
+                return new Vector2(bounds.center.x, bounds.max.y + _bulletOffset);
+            else if (angle > -135 && angle < -45) //Down
                 return new Vector2(bounds.center.x, bounds.min.y - _bulletOffset);
+            else //Left
+                return new Vector2(bounds.min.x - _bulletOffset, bounds.center.y);
 
         }
+
+        public void SetInputActions(InputActions inputs) => InputActions = inputs;
     }
 }
